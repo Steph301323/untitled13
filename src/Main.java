efd3455e33c99c70dee9f35ec42040ea678706ea
karsev ivan <k.ivan.19@yandex.ru>
2021-05-16 14:12:02 +0300

    text

--- src/Main.java
+++ src/Main.java
@@ -5,89 +5,80 @@ import java.io.FileWriter;
 
 class Main {
 
-    private static Object File;
+   
 
     public static void main(String[] args) {
 
+        StringBuilder sb =new StringBuilder() ;
+
         File dir = new File("D://Games");
-        // если объект представляет каталог
-        if (dir.isDirectory()) {
-            // получаем все вложенные объекты в каталоге
-            for (File item : dir.listFiles()) {
+        if (dir.mkdir())
+            System.out.println(sb);
 
-                if (item.isDirectory()) {
-
-                    System.out.println(item.getName() + "  \t folder");
-                } else {
-
-                    System.out.println(item.getName() + "\t file");
-                }
-            }
-        }
         File directory = new File("D://Games//src//main");
         boolean created = directory.mkdir();
         if (created)
-            System.out.println("директория создана");
-        File file1 = new File("D://Games//src//test");
-        boolean d = file1.mkdir();
-        if (d)
-            System.out.println("директория создана");
+            System.out.println(sb);
+        File test = new File("src/test");
+        boolean testim = test.mkdir();
+        if (testim)
+            System.out.println(sb);
 
         File file = new File("D://Games//res");
 
-        boolean a = file.mkdir();
-        if (a)
-            System.out.println("директория создана");
+        boolean alex = file.mkdir();
+        if (alex)
+            System.out.println(sb);
         File direct = new File("D://Games//savegames");
-        boolean b = direct.mkdir();
-        if (b)
-            System.out.println("директория создана");
+        boolean box = direct.mkdir();
+        if (box)
+            System.out.println(sb);
         File dire = new File("D://Games//temp");
-        boolean c = dire.mkdir();
+        boolean cex = dire.mkdir();
 
-        if (c)
-            System.out.println("директория создана");
+        if (cex)
+            System.out.println(sb);
 
-        File myFile = new File("D://Games//src//main//Main.java");
+        File myFile = new File("main/Main.java");
         try {
             if (myFile.createNewFile())
-                System.out.println("Файл был создан");
+                System.out.println(sb);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-        File myFile1 = new File("D://Games//src//main//Utils.java");
+        File utils = new File("main/Utils.java");
         try {
-            if (myFile1.createNewFile())
-                System.out.println("Файл был создан");
+            if (utils.createNewFile())
+                System.out.println(sb);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
-        File file3 = new File("D://Games//res//drawables");
+        File draw = new File("res/drawables");
 
-        boolean aa = file3.mkdir();
-        if (aa)
-            System.out.println("Folder has been created");
-        File file4 = new File("D://Games//res//vektors");
+        boolean fly = draw.mkdir();
+        if (fly)
+            System.out.println(sb);
+        File vector = new File("res/vektors");
 
-        boolean aaa = file4.mkdir();
-        if (aaa)
-            System.out.println("директория создана");
-        File file5 = new File("D://Games//res//icons");
+        boolean vect = vector.mkdir();
+        if (vect)
+            System.out.println(sb);
+        File icons = new File("D://Games//res//icons");
 
-        boolean bb = file5.mkdir();
-        if (bb)
-            System.out.println("директория создана");
+        boolean icon = icons.mkdir();
+        if (icon)
+            System.out.println(sb);
 
-        File myFile12 = new File("D://Games//temp//temp.txt");
+        File temp = new File("temp/temp.txt");
         try {
-            if (myFile12.createNewFile())
-                System.out.println("Файл был создан");
+            if (temp.createNewFile())
+                System.out.println(sb);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(File);
+
+        sb.append(dir);
         String text = sb.toString();
         try (FileWriter writer = new FileWriter("temp.txt", false)) {
             writer.write(text);
@@ -99,4 +90,6 @@ class Main {
         }
 
     }
+
+
 }
